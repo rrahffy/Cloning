a053ce530ab2bfac57301b0af52f950645f31d88
aellisha <angelica.ilaica7@gmail.com>
2025-05-17 23:58:23 +0800

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,13 +1,41 @@
+/**
+	This is the abstract class for all the rooms. It contains what elements are similar in the rooms. 
+	
+	@author Maria Angelica Muñoz (243172) and Rafael Jack Rafanan (246338)
+	@version 20 May 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
+
 import java.awt.*;
 import java.util.*;
 
 public abstract class Room{
     private ArrayList<Item> items;
 
+    /**
+     * Each room will have an ArrayList of items
+     * @param items The items that makes up the room
+     */
     public Room(ArrayList<Item> items){
         this.items = items;
     }
 
+    /**
+     * For drawing the room's items 
+     * @param g Will help draw the images
+     */
     public void drawRoom(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         for(Item item : items){
@@ -15,9 +43,21 @@ public abstract class Room{
         }
     }
 
+    /**
+     * Gets the items that makes up the rooms
+     * @return items The items inside the ArrayList
+     */
     public ArrayList<Item> getItems(){
         return items;
     } 
+
+    /**
+     * Updates the rooms based on the new items in it
+     * @param newRoomItems What new items will be added
+     */
+    public void updateRoom(ArrayList<Item> newRoomItems){
+        this.items = newRoomItems;
+    }
 
     // public void checkCollision(Player player){
 
